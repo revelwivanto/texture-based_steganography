c7ada7c1255377f7915f760dd62cf046e7ecd663
revelwivanto <116476269+revelwivanto@users.noreply.github.com>
2024-08-27 16:28:06 +0700

    Update image_processing.py

--- image_processing.py
+++ image_processing.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 import os
+from PIL import Image
+from collections import Counter
 
 def calculate_ratios(image_path, output_dir):
     # Read the image
@@ -30,8 +32,8 @@ def calculate_ratios(image_path, output_dir):
     _, binary = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)
 
     # Detect edges to find boundaries
-    edges = cv2.Sobel(image_copy2, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5) # Combined X and Y Sobel Edge Detection
-    
+    # edges = cv2.cvtColor(cv2.Sobel(image_copy2, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5), cv2.COLOR_BGR2GRAY) # Combined X and Y Sobel Edge Detection
+    edges = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # Create output images for object, boundary, and background
     background = np.ones_like(binary) * 255 - binary - edges
     
@@ -43,8 +45,31 @@ def calculate_ratios(image_path, output_dir):
     cv2.imwrite(os.path.join(output_dir, 'boundary.png'), edges)
     cv2.imwrite(os.path.join(output_dir, 'background.png'), background)
     
+def count_pixel_values(image_path):
+    # Open the image
+    img = Image.open(image_path)
+        
+    # Get the pixel values
+    pixel_values = list(img.getdata())
+    
+    # Count the pixel values
+    pixel_count = Counter(pixel_values)
+
+    # Sort the pixel count by pixel value
+    sorted_pixel_count = dict(sorted(pixel_count.items()))
+    
+    return sorted_pixel_count
+
+  
 # Example usage
 image_path = 'Original_Dataset/Lena.png'
 output_dir = 'Original_Dataset'
 
 calculate_ratios(image_path, output_dir)
+
+image_path = 'Original_Dataset/boundary.png'
+pixel_count = count_pixel_values(image_path)
+
+# Print the pixel count
+for pixel_value, count in pixel_count.items():
+    print(f"Pixel value {pixel_value}: {count} times")  
